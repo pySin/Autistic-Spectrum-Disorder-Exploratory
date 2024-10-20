fa5ea08f1c1854b14dde4b6da0b0fe31e412dd47
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-10-20 12:10:49 +0100

    Save code

--- import_SFARI_ASD_data.py
+++ import_SFARI_ASD_data.py
@@ -3,3 +3,9 @@ import mysql.connector
 
 FILE_PATH = r"C:\Users\1\PycharmProjects\Autistic-Spectrum-Disorder-Exploratory\data\SFARI_dataset.csv"
 
+
+def read_first_line(file_path):
+    with open(file_path, "r") as o_file:
+        first_line = o_file.readline().split(",")
+    return first_line
+
